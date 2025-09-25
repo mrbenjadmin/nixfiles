c207e188ee4fd4763bfa999eb6ab4a61293806db
mrbenjadmin <ben.strachan1@gmail.com>
2025-09-25 17:03:29 -0400

    zfs specific kernel deprecation

--- machines/brunhilde/zfs.nix
+++ machines/brunhilde/zfs.nix
@@ -1,10 +1,10 @@
-{ config, ... }:
+{ pkgs, ... }:
 
 {
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
   networking.hostId = "912862a4";
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages;
   boot.zfs.extraPools = [ "importantdata" "bigdata" ];
   services.zfs.autoScrub.enable = true;
 }

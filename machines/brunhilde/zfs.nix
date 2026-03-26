f44d07da1937ec87d09bad376b2e313a4289352b
mrbenjadmin <ben.strachan1@gmail.com>
2026-03-26 03:52:16 -0400

    zfs

--- machines/brunhilde/zfs.nix
+++ machines/brunhilde/zfs.nix
@@ -4,7 +4,6 @@
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
   networking.hostId = "912862a4";
-  boot.kernelPackages = pkgs.linuxPackages;
-  boot.zfs.extraPools = [ "importantdata" "bigdata" ];
+  #boot.zfs.extraPools = [ "importantdata" "bigdata" ];
   services.zfs.autoScrub.enable = true;
 }
